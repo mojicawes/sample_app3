337ae4916d1c1453ed5465efbe5a214745e8bf8d
Westley Mojica <zaa930@my.utsa.edu>
2011-11-08 09:51:26 -0600

    Updated Gemfile

---
+++ My Documents/Aptana Studio 3 Workspace/sample_app3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp3::Application.config.secret_token = 'b2c8dd70b6c1757af35b8638ba3ce83c263c11df55dc94d80e00501e589596c21a6aae5658cc56757ecda6f53176202348ba87e761765009457a36e95e9f8e8a'
